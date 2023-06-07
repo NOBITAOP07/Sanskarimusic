37e6e9588465ef73afb81e06e27a8c550274eb59
SanskariNobita <130442021+SanskariNobita@users.noreply.github.com>
2023-06-07 20:54:06 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -20,7 +20,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def gen_thumb(videoid, user_id, chat_id):
+async def gen_thumb(videoid, user_id, group_id):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
 
@@ -58,8 +58,8 @@ async def gen_thumb(videoid, user_id, chat_id):
                     await f.close()
 
         try:
-            wxyz = await app.get_profile_photos(chat_id)
-            wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{chat_id}.jpg')
+            wxyz = await app.get_profile_photos(group_id)
+            wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{group_id}.jpg')
         except:
             hehe = await app.get_profile_photos(app.id)
             wxy = await app.download_media(hehe[0]['file_id'], file_name=f'{app.id}.jpg')
@@ -134,7 +134,7 @@ async def gen_thumb(videoid, user_id, chat_id):
     except Exception:
         return YOUTUBE_IMG_URL
 
-async def gen_qthumb(videoid, chat_id):
+async def gen_qthumb(videoid, group_id):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
 
